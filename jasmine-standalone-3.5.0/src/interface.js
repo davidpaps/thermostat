38defc7e0adabbccc0b21cd889ae5cdf23ecfc11
David Papamichael <davidpapamichael@gmail.com>
2020-01-23 17:10:25 +0000

    colour change of temp working via energyUsge method

--- jasmine-standalone-3.5.0/src/interface.js
+++ jasmine-standalone-3.5.0/src/interface.js
@@ -2,32 +2,32 @@
 $(document).ready(function() {  
 
   var thermostat = new Thermostat();
-
-  $('#temperature').text(thermostat.temp());
+  updateTemperature();
 
   $('#increase').click(function() {
     thermostat.increaseTemp();
-    $('#temperature').text(thermostat.temp());
+    updateTemperature();
   }) 
 
   $('#decrease').click(function() {
     thermostat.decreaseTemp();
-    $('#temperature').text(thermostat.temp());
+    updateTemperature();
   }) 
 
   $('#reset').click(function() {
     thermostat.resetTemp();
-    $('#temperature').text(thermostat.temp());
+    updateTemperature();
   }) 
 
   $('#power').click(function() {
     thermostat.togglePowerSaving();
-    $('#temperature').text(thermostat.temp());
+    updateTemperature();
 
   }) 
-})
 
-function temp() {
-  $('#temperature').text(thermostat.temp());
-  $('#temperature').attr('class', thermostat.energyUsage());
-}
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temp());
+    $('#temperature').attr('class', thermostat.energyUsage());
+  }
+
+})
